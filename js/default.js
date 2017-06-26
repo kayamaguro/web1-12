a45f48e89673ac9e6857b439a80b61f85240ae99
kayamaguro <g1627017@st.t-kougei.ac.jp>
2017-06-26 18:06:47 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,8 +3,18 @@ window.alert(document.getElementById('form').word.value);
 }
 
 for(var i=1; i<100; i++){
-  if(var i% 3 == 0){}
+  if(var i% 3 === 0 && i%5 !==0){
+  console.log('Fizz');
+  }else if(i%3 !==0 && i%5 ===0){
+  console.log('Buzz');
+  }else if(i%3 ===0 && i%5 ===0){
+  console.log('FizzBuzz');
+  }else{
+  console.log(i);
+  }
 var li=document.createElement('li');
   li.textContent=i+'回目';
 document.getElementById('list').appendChild(li);
 }
+
+
