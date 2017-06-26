3e6d5b6a39a07bcf69dc04b57ad7e714a86d2f53
kayamaguro <g1627017@st.t-kougei.ac.jp>
2017-06-26 18:21:00 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,16 +1,11 @@
-<script>
-    function fizzbuzz(){
-        for(i=1; i<=100; i++){
-            if(i%3 === 0 && i%5 !== 0){
-                console.log('Fizz');
-            }else if(i%3 !== 0 && i%5 === 0){
-                console.log('Buzz');
-            }else if(i%3 === 0 && i%5 === 0){
-                console.log('FizzBuzz');
-            }else{
-                console.log(i);
-            }
-        }
-    }
-    fizzbuzz();
-</script>
+for(var i = 1; i < 100; i++){
+var li = document.createElement('li');
+if(i % 3 == 0 && i % 5 == 0)
+li.textContent = 'FizzBuzz';
+else if(i % 3 == 0)
+li.textContent = 'Fizz';
+else if(i % 5 == 0)
+li.textContent = 'Buzz';
+else
+    li.textContent = i+1 '回数';
+document.getElementById('list').appendChild();}
