a60c71b7464b5072cbaba3b63b9edb38c627410f
kayamaguro <g1627017@st.t-kougei.ac.jp>
2017-06-26 18:10:28 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,20 +1,16 @@
-document.getElementById('form').onsubmit=function(){
-window.alert(document.getElementById('form').word.value);
-}
-
-for(var i=1; i<100; i++){
-  if(var i% 3 === 0 && i%5 !==0){
-  console.log('Fizz');
-  }else if(i%3 !==0 && i%5 ===0){
-  console.log('Buzz');
-  }else if(i%3 ===0 && i%5 ===0){
-  console.log('FizzBuzz');
-  }else{
-  console.log(i);
-  }
-var li=document.createElement('li');
-  li.textContent=i+'回目';
-document.getElementById('list').appendChild(li);
-}
-
-
+<script>
+    function fizzbuzz(){
+        for(i=1; i<=100; i++){
+            if(i%3 === 0 && i%5 !== 0){
+                console.log('Fizz');
+            }else if(i%3 !== 0 && i%5 === 0){
+                console.log('Buzz');
+            }else if(i%3 === 0 && i%5 === 0){
+                console.log('FizzBuzz');
+            }else{
+                console.log(i);
+            }
+        }
+    }
+    fizzbuzz();
+</script>
