bb5543ddf592fef85b66f86ee9cbbce7dd5991d8
kayamaguro <g1627017@st.t-kougei.ac.jp>
2017-06-26 17:18:03 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,7 +2,7 @@ document.getElementById('form').onsubmit=function(){
 window.alert(document.getElementById('form').word.value);
 }
 
-for(var i=1; i<10; i++){
+for(var i=1; i<100; i++){
 var li=document.createElement('li');
   li.textContent=i+'回目';
 document.getElementById('list').appendChild(li);
